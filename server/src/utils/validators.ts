4327d8d38ca25e403642235cbb8525b3df5ef932
Rafi Rajoyan <rafirjr@gmail.com>
2024-04-26 16:59:40 -0700

    Added controller folder with auth.ts and user.ts along with validator file for data validation

---
+++ server/src/utils/validators.ts
@@ -0,0 +1,48 @@
+interface AuthErrors {
+    username?: string;
+    password?: string;
+}
+
+export const registerValidator = (username: string, password: string) => {
+    const errors: AuthErrors = {};
+
+    if (
+        !username ||
+        username.trim() === "" ||
+        username.length > 20 ||
+        username.length < 3
+    ) {
+        errors.username =
+            "Username must be in the range of 3-20 characters in length.";
+    }
+
+    if (!/^[a-zA-Z0-9-_]*$/.test(username)) {
+        errors.username = "Username must have alphanumeric characters only.";
+    }
+
+    if (!password || password.length < 6) {
+        errors.password = "Password must be at least 6 characters long.";
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1,
+    };
+};
+
+export const loginValidator = (username: string, password: string) => {
+    const errors: AuthErrors = {};
+
+    if (!username || username.trim() === "") {
+        errors.username = "Username field must not be empty.";
+    }
+
+    if (!password) {
+        errors.password = "Password field must not be empty.";
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1,
+    };
+};
