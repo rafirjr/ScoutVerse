621657ae7ca45fa72dead12d68379f73cf47ee3d
Rafi Rajoyan <rafirjr@gmail.com>
2024-05-09 12:52:14 -0700

    Added user routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "express-async-errors";
 import middleware from "./middleware";
 import authRoutes from "./routes/auth";
+import userRoutes from "./routes/user";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", authRoutes);
+app.use("/users", userRoutes);
 
 app.use(middleware.unknownEndPointHandler);
 app.use(middleware.errorHandler);
