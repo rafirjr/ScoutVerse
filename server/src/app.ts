fe3d5378fba3fcbeb0427893f0d288728cb86536
Rafi Rajoyan <rafirjr@gmail.com>
2024-05-09 10:19:01 -0700

    Added auth route for signup and login and added to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import "express-async-errors";
 import middleware from "./middleware";
+import authRoutes from "./routes/auth";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", authRoutes);
 
 app.use(middleware.unknownEndPointHandler);
 app.use(middleware.errorHandler);
