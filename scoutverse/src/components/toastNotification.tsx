f241e75f19e61bfd1e0e78bd2bd78708a8c5ed03
Rafi Rajoyan <rafirjr@gmail.com>
2024-08-24 21:29:13 -0700

    Created toast notification component and added it to Dashboard page

---
+++ scoutverse/src/components/toastNotification.tsx
@@ -0,0 +1,45 @@
+import { useSelector } from "react-redux";
+import { useAppDispatch } from "../redux/hooks";
+import {
+    selectNotifState,
+    clearNotification,
+} from "../redux/slices/notificationSlice";
+import { Toast } from "flowbite-react";
+import { HiCheck, HiExclamation } from "react-icons/hi";
+
+const ToastNotification = () => {
+    const dispatch = useAppDispatch();
+    const { message, type } = useSelector(selectNotifState);
+
+    const clearNotif = () => {
+        dispatch(clearNotification());
+    };
+
+    if (!message || !type) return null;
+
+    return (
+        <div className="flex flex-col gap-4">
+            <Toast>
+                {type == "success" ? (
+                    <>
+                        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                            <HiCheck className="h-5 w-5" />
+                        </div>
+                        <div>{message}</div>
+                        <Toast.Toggle onClick={clearNotif} />
+                    </>
+                ) : (
+                    <>
+                        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                            <HiExclamation className="h-5 w-5" />
+                        </div>
+                        <div>{message}</div>
+                        <Toast.Toggle onClick={clearNotif} />
+                    </>
+                )}
+            </Toast>
+        </div>
+    );
+};
+
+export default ToastNotification;
--- scoutverse/src/pages/Dashboard.tsx
+++ scoutverse/src/pages/Dashboard.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Sidebar from "../components/sidebar";
 import Topnav from "../components/topnav";
+import ToastNotification from "../components/toastNotification";
 
 const Dashboard: React.FC = () => {
     return (
         <div className="min-h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
             <Topnav />
             <Sidebar />
+            <div className="container mx-auto h-10"></div>
+            <div className="container mx-auto h-10"></div>
+            <div className="container mx-auto h-10"></div>
+            <ToastNotification />
         </div>
     );
 };
