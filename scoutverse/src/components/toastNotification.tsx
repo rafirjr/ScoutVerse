2d7a4d90eca297921a897f17942fd61fe65631df
Rafi Rajoyan <rafirjr@gmail.com>
2024-09-04 12:00:26 -0700

    Fixed error notification

--- scoutverse/src/components/toastNotification.tsx
+++ scoutverse/src/components/toastNotification.tsx
@@ -20,7 +20,7 @@ const ToastNotification = () => {
     return (
         <div className="flex flex-col gap-4">
             <Toast>
-                {type == "success" ? (
+                {type === "success" ? (
                     <>
                         <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
                             <HiCheck className="h-5 w-5" />
@@ -30,7 +30,7 @@ const ToastNotification = () => {
                     </>
                 ) : (
                     <>
-                        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200">
                             <HiExclamation className="h-5 w-5" />
                         </div>
                         <div className="p-2">{message}</div>
