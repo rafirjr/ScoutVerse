0638f82fc0e9024a787b6fb379c1422f7718bfd2
Rafi Rajoyan <rafirjr@gmail.com>
2024-09-11 11:15:33 -0700

    Made table scrollable for smaller screen sizes

--- scoutverse/src/components/rosterTable.tsx
+++ scoutverse/src/components/rosterTable.tsx
@@ -32,48 +32,57 @@ const RosterTable: React.FC<RosterTableProps> = ({ scouts }) => {
         <div className="flex">
             <div className="flex-col lg:w-1/4"></div>
             <div className=" bg-white rounded-xl mx-auto shadow-lg overflow-hidden w-5/6 h-fit md:w-3/4 lg:mr-36">
-                <Table striped>
-                    <Table.Head>
-                        <Table.HeadCell>First Name</Table.HeadCell>
-                        <Table.HeadCell>Last Name</Table.HeadCell>
-                        <Table.HeadCell>Khoump</Table.HeadCell>
-                        <Table.HeadCell>DOB</Table.HeadCell>
-                        {/* <Table.HeadCell>Address</Table.HeadCell> */}
-                        <Table.HeadCell>Parent Name</Table.HeadCell>
-                        <Table.HeadCell>Parent Email</Table.HeadCell>
-                        <Table.HeadCell>Parent Number</Table.HeadCell>
-                        <Table.HeadCell>Details</Table.HeadCell>
-                    </Table.Head>
-                    <Table.Body className="divide-y">
-                        {scouts.map((scout, index) => (
-                            <Table.Row key={index}>
-                                <Table.Cell>{scout.first_name}</Table.Cell>
-                                <Table.Cell>{scout.last_name}</Table.Cell>
-                                <Table.Cell>{scout.khoump}</Table.Cell>
-                                <Table.Cell>
-                                    {scout.date_of_birth
-                                        .toString()
-                                        .substring(0, 10)}
-                                </Table.Cell>
-                                {/* <Table.Cell>
+                <div className="overflow-x-auto w-full">
+                    <Table striped>
+                        <Table.Head>
+                            <Table.HeadCell>First Name</Table.HeadCell>
+                            <Table.HeadCell>Last Name</Table.HeadCell>
+                            <Table.HeadCell>Khoump</Table.HeadCell>
+                            <Table.HeadCell>DOB</Table.HeadCell>
+                            {/* <Table.HeadCell>Address</Table.HeadCell> */}
+                            <Table.HeadCell>Parent Name</Table.HeadCell>
+                            <Table.HeadCell>Parent Email</Table.HeadCell>
+                            <Table.HeadCell>Parent Number</Table.HeadCell>
+                            <Table.HeadCell>Details</Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            {scouts.map((scout, index) => (
+                                <Table.Row key={index}>
+                                    <Table.Cell>{scout.first_name}</Table.Cell>
+                                    <Table.Cell>{scout.last_name}</Table.Cell>
+                                    <Table.Cell>{scout.khoump}</Table.Cell>
+                                    <Table.Cell>
+                                        {scout.date_of_birth
+                                            .toString()
+                                            .substring(0, 10)}
+                                    </Table.Cell>
+                                    {/* <Table.Cell>
                                     {`${scout.street} \n ${scout.city}, ${scout.state} ${scout.zip_code}`}
                                 </Table.Cell> */}
-                                <Table.Cell>{scout.parent_name}</Table.Cell>
-                                <Table.Cell>{scout.parent_email}</Table.Cell>
-                                <Table.Cell>{scout.parent_number}</Table.Cell>
-                                <Button
-                                    className="mx-auto mt-2"
-                                    pill
-                                    size="sm"
-                                    color="blue"
-                                    onClick={() => handleViewScout(scout.id)}
-                                >
-                                    <IoEyeOutline size={20} />
-                                </Button>
-                            </Table.Row>
-                        ))}
-                    </Table.Body>
-                </Table>
+                                    <Table.Cell>{scout.parent_name}</Table.Cell>
+                                    <Table.Cell>
+                                        {scout.parent_email}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {scout.parent_number}
+                                    </Table.Cell>
+                                    <Button
+                                        className="mx-auto mt-2"
+                                        pill
+                                        size="sm"
+                                        color="blue"
+                                        onClick={() =>
+                                            handleViewScout(scout.id)
+                                        }
+                                    >
+                                        <IoEyeOutline size={20} />
+                                    </Button>
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                </div>
+
                 <Modal show={openModal} onClose={() => setOpenModal(false)}>
                     {scoutState.currentScoutID ? (
                         <ScoutInfoModal
